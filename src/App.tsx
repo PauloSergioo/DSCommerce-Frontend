0aa6dc5df53050b24c07343f4334ab4eb74fc5e0
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-26 14:45:17 -0300

    PrivateRoute

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,31 @@ import Catalog from "./routes/ClientHome/Catalog";
 import Login from "./routes/ClientHome/Login";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import { ContextCartCount } from "./utils/context-cart";
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-import {history} from './utils/history';
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { history } from "./utils/history";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
 
   return (
     <ContextCartCount.Provider
-      value={{ contextCartCount, setContextCartCount }} >
+      value={{ contextCartCount, setContextCartCount }}
+    >
       <HistoryRouter history={history}>
         <Routes>
           <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
-            <Route path="product-details/:productId" element={<ProductDetails />} />
+            <Route
+              path="product-details/:productId"
+              element={<ProductDetails />}
+            />
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
           </Route>
-          <Route path="/admin/" element={<Admin />}>
-            <Route index element={<AdminHome/>} />
+          <Route path="/admin/" element={<PrivateRoute><Admin /></PrivateRoute>}>
+            <Route index element={<AdminHome />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
