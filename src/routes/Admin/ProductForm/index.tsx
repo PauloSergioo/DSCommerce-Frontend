2bbe5671c8e3d884d0eae20ea21020d0dc101f48
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-31 14:18:19 -0300

    Admin area routes

---
+++ src/routes/Admin/ProductForm/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ProductForm() {
+
+    return(
+
+        <h1>Formulario de produtos</h1>
+    );
+
+}
---
+++ src/routes/Admin/ProductListing/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ProductListing() {
+
+    return(
+
+        <h1>Listagem de produtos</h1>
+    );
+
+}
